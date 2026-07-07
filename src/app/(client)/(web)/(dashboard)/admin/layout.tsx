d4866c130026299c1d77e1b4777a9cce4fb2bb0d
UmarAbdullah79 <uu0339843@gmail.com>
2026-07-07 17:48:18 +0530

    file setup

---
+++ src/app/(client)/(web)/(dashboard)/admin/layout.tsx
@@ -0,0 +1,17 @@
+// import TopBar from "@/src/components/TopBar";
+// import Navbar from "@/src/components/Navbar";
+// import Footer from "@/src/components/Footer";
+// import FloatingButtons from "@/src/components/FloatingButtons";
+
+export default function WebLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Top bar scrolls away, Navbar sticks */}
+      {/* <TopBar />
+      <Navbar />
+      <FloatingButtons /> */}
+      <main className="grow">{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
